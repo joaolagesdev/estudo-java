33a730d0240cf7f15217d40388b454079d6af445
João Artur <67400301+joaothomaz@users.noreply.github.com>
2021-02-17 21:05:39 -0300

    Aula 10 e Aula 11
    Herança

---
+++ Aula11/src/aula11/Bolsista.java
@@ -0,0 +1,25 @@
+
+package aula11;
+
+public class Bolsista extends Aluno {
+    private float bolsa;
+    
+    public void renovarBolsa() {
+        System.out.println("Renovando bolsa de " + this.nome);
+    }
+    
+    @Override
+    public void pagarMensalidade() {
+        System.out.println(this.nome + " é bolsista! Pagamento OK! ");
+    }
+
+    public float getBolsa() {
+        return bolsa;
+    }
+
+    public void setBolsa(float bolsa) {
+        this.bolsa = bolsa;
+    }
+    
+    
+}
